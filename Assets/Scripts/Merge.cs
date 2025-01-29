126edfe62b0b33727650b318c83977b8ce0b0031
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-01-29 13:28:00 +0100

    Function Jijijaja
    jijijaja return true if jiji and jaja are the same bool, any option beside that returns false

--- Assets/Scripts/Merge.cs
+++ Assets/Scripts/Merge.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Merge : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] bool jiji;
+    [SerializeField] bool jaja;
+
+
     void Start()
     {
-        
+        print("Los Skibidis ganaron a los toilets");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print("ESto es una prueba muy segura");
+    }
+
+    private bool JijiJAJA()
+    {
+        if (jiji && jaja) return true;
+        else if (!jiji && jaja) return false;
+        else if (jiji && !jaja) return false;
+        else if (!jiji && !jaja) return true;
+        return false;
     }
 }
