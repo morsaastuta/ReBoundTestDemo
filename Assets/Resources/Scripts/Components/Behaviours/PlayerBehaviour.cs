7e9de647356ce145dd81d2cb2abdd00903f02dc1
Morsa <167016181+morsaastuta@users.noreply.github.com>
2025-02-11 13:34:58 +0100

    Input update
    - New Player prefab created.
    - Handtracking refactored and optimized.
    - Conroller tracking and input refactored and optimized.

--- Assets/Resources/Scripts/Components/Behaviours/PlayerBehaviour.cs
+++ Assets/Resources/Scripts/Components/Behaviours/PlayerBehaviour.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,62 +5,99 @@ using static Glossary;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [Header("Common references")]
+    [SerializeField] Transform eyes;
     [SerializeField] public GameMode gameMode;
     [SerializeField] List<Ball> demoBalls = new();
 
-    [Header("Hands references")]
-    [SerializeField] List<GameObject> handsExclusive = new();
-
-    [Header("Controllers references")]
-    [SerializeField] List<GameObject> controllersExclusive = new();
-    [SerializeField] GloveBehaviour C_glove;
-    [SerializeField] GameObject C_grabInteractorL;
-    [SerializeField] GameObject C_distanceGrabInteractorL;
-    [SerializeField] GameObject C_grabInteractorR;
-    [SerializeField] GameObject C_distanceGrabInteractorR;
-    [SerializeField] AudioSource C_audioSource;
-
-    [Header("Desktop references")]
-    [SerializeField] List<GameObject> desktopExclusive = new();
-    [SerializeField] Rigidbody D_body;
-    [SerializeField] AudioSource D_audioSource;
-
+    // Commons
+    bool shot = false;
     List<Ball> balls = new();
     int selectedBall = 0;
     List<GameObject> auxiliarBalls = new();
-    bool shot = false;
+    AudioSource audioSource;
 
-    AudioSource currentSource;
+    // VR commons
+    GameObject projection;
+    Transform wrist;
+    Transform palm;
+    bool aiming = false;
 
     // Desktop exclusive
     float speed = 2;
     float velForward;
     float velSide;
 
+    [Header("Hands references")]
+    [SerializeField] List<GameObject> handsExclusives = new();
+    [SerializeField] GameObject H_projection;
+    [SerializeField] Transform H_wrist;
+    [SerializeField] Transform H_palm;
+    [SerializeField] OVRHand H_hand;
+    [SerializeField] AudioSource H_audioSource;
+
+    [Header("Controllers references")]
+    [SerializeField] List<GameObject> controllersExclusives = new();
+    [SerializeField] GameObject C_projection;
+    [SerializeField] Transform C_wrist;
+    [SerializeField] Transform C_palm;
+    [SerializeField] AudioSource C_audioSource;
+    [SerializeField] GameObject C_grabInteractorL;
+    [SerializeField] GameObject C_distanceGrabInteractorL;
+    [SerializeField] GameObject C_grabInteractorR;
+    [SerializeField] GameObject C_distanceGrabInteractorR;
+
+    [Header("Desktop references")]
+    [SerializeField] List<GameObject> desktopExclusives = new();
+    [SerializeField] GameObject D_projection;
+    [SerializeField] Transform D_wrist;
+    [SerializeField] Transform D_palm;
+    [SerializeField] AudioSource D_audioSource;
+    [SerializeField] Rigidbody D_body;
+
     void Start()
     {
         SetGameMode(gameMode);
+
+        // Add demo balls
+        foreach (Ball ball in demoBalls) ObtainBall(ball);
+
+        // Set default projection using first ball if it exists
+        if (balls.Count < 0) projection.GetComponent<ProjectionBehaviour>().UpdateBall(balls[selectedBall]);
     }
 
     void SetGameMode(GameMode gm)
     {
         gameMode = gm;
 
-        foreach (GameObject go in handsExclusive) go.SetActive(false);
-        foreach (GameObject go in controllersExclusive) go.SetActive(false);
+        foreach (GameObject go in handsExclusives) go.SetActive(false);
+        foreach (GameObject go in controllersExclusives) go.SetActive(false);
+        foreach (GameObject go in desktopExclusives) go.SetActive(false);
 
         switch (gm)
         {
-            case GameMode.Hands:
-                foreach (GameObject go in handsExclusive) go.SetActive(true);
-                break;
             case GameMode.Controllers:
-                foreach (GameObject go in controllersExclusive) go.SetActive(true);
-                currentSource = C_audioSource;
+                foreach (GameObject go in controllersExclusives) go.SetActive(true);
+                projection = C_projection;
+                wrist = C_wrist;
+                palm = C_palm;
+                audioSource = C_audioSource;
                 break;
+
+            case GameMode.Hands:
+                foreach (GameObject go in handsExclusives) go.SetActive(true);
+                projection = H_projection;
+                wrist = H_wrist;
+                palm = H_palm;
+                audioSource = H_audioSource;
+                break;
+
             case GameMode.Desktop:
-                foreach (GameObject go in desktopExclusive) go.SetActive(true);
-                currentSource = D_audioSource;
+                foreach (GameObject go in desktopExclusives) go.SetActive(true);
+                projection = D_projection;
+                wrist = D_wrist;
+                palm = D_palm;
+                audioSource = D_audioSource;
                 break;
         }
     }
@@ -69,55 +105,190 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameMode != GameMode.Hands)
+        // Controllers exclusive
+        if (gameMode == GameMode.Controllers)
         {
-            // Controllers exclusive
-            if (gameMode == GameMode.Controllers)
+            if (InputManager.instance.Holding(InputManager.instance.distanceGrabL))
             {
-                if (InputManager.instance.Holding(InputManager.instance.distanceGrabL))
-                {
-                    C_distanceGrabInteractorL.SetActive(true);
-                    C_grabInteractorL.SetActive(false);
-                }
-                else
-                {
-                    C_distanceGrabInteractorL.SetActive(false);
-                    C_grabInteractorL.SetActive(true);
-                }
-
-                if (InputManager.instance.Holding(InputManager.instance.distanceGrabR))
-                {
-                    C_distanceGrabInteractorR.SetActive(true);
-                    C_grabInteractorR.SetActive(false);
-                }
-                else
-                {
-                    C_distanceGrabInteractorR.SetActive(false);
-                    C_grabInteractorR.SetActive(true);
-                }
-
-                if (InputManager.instance.Holding(InputManager.instance.shoot)) C_glove.Shoot();
-
-                if (InputManager.instance.Holding(InputManager.instance.aim)) C_glove.aimBeam.Cast(balls[selectedBall]);
-                else C_glove.aimBeam.Clear();
-
-                if (InputManager.instance.Pressed(InputManager.instance.swap)) C_glove.SwitchBall(true);
-
-                if (InputManager.instance.Pressed(InputManager.instance.clear)) C_glove.ClearAuxiliars();
+                C_distanceGrabInteractorL.SetActive(true);
+                C_grabInteractorL.SetActive(false);
             }
-            // Desktop exclusive
-            else if (gameMode == GameMode.Desktop)
+            else
             {
-                Vector2 vMove = InputManager.instance.Inclination(InputManager.instance.move);
-                velSide = vMove.x;
-                velForward = vMove.y;
-
-                Vector2 vLook = InputManager.instance.Inclination(InputManager.instance.look);
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + vLook.x, transform.rotation.eulerAngles.z);
+                C_distanceGrabInteractorL.SetActive(false);
+                C_grabInteractorL.SetActive(true);
             }
-            else return;
+
+            if (InputManager.instance.Holding(InputManager.instance.distanceGrabR))
+            {
+                C_distanceGrabInteractorR.SetActive(true);
+                C_grabInteractorR.SetActive(false);
+            }
+            else
+            {
+                C_distanceGrabInteractorR.SetActive(false);
+                C_grabInteractorR.SetActive(true);
+            }
+
+            if (InputManager.instance.Holding(InputManager.instance.shoot)) Shoot();
+
+            if (InputManager.instance.Holding(InputManager.instance.aim)) palm.GetComponent<AimBeam>().Cast(balls[selectedBall]);
+            else palm.GetComponent<AimBeam>().Clear();
+
+            if (InputManager.instance.Pressed(InputManager.instance.swap)) SwitchBall(true);
+
+            if (InputManager.instance.Pressed(InputManager.instance.clear)) ClearAuxiliars();
+        }
+        // Hands exclusive
+        else if (gameMode == GameMode.Hands)
+        {
+            // While aiming...
+            if (aiming)
+            {
+                // If no ball was recently shot...
+                if (!shot)
+                {
+                    // Update the aim beam rendering
+                    if (Vector3.Angle(eyes.forward, palm.forward) <= 90) palm.GetComponent<AimBeam>().Cast(balls[selectedBall]);
+                    else palm.GetComponent<AimBeam>().Clear();
+
+                    // Pinch index finger to shoot ball
+                    if (H_hand.GetFingerIsPinching(OVRHand.HandFinger.Index)) Shoot();
+                }
+                else palm.GetComponent<AimBeam>().Clear();
+
+                // Pinch middle finger to delete all auxiliar balls
+                if (H_hand.GetFingerIsPinching(OVRHand.HandFinger.Middle)) ClearAuxiliars();
+            }
+            else palm.GetComponent<AimBeam>().Clear();
+        }
+        // Desktop exclusive
+        else if (gameMode == GameMode.Desktop)
+        {
+            Vector2 vMove = InputManager.instance.Inclination(InputManager.instance.move);
+            velSide = vMove.x;
+            velForward = vMove.y;
+
+            Vector2 vLook = InputManager.instance.Inclination(InputManager.instance.look);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + vLook.x, transform.rotation.eulerAngles.z);
         }
     }
+
+    public void Shoot()
+    {
+        if (!shot) StartCoroutine(Shot());
+    }
+
+    public IEnumerator Shot()
+    {
+        Preview(false, false);
+        shot = true;
+
+        GameObject ball = Instantiate(balls[selectedBall].prefab);
+
+        // Set ball
+        ball.GetComponent<BallBehaviour>().ball = Instantiate(balls[selectedBall]);
+
+        // If ball is auxiliar, track it
+        if (ball.GetComponent<BallBehaviour>().ball.auxiliar) auxiliarBalls.Add(ball);
+
+        // Set position (Palm)
+        ball.transform.position = palm.position;
+
+        // Set rotation (Palm or Gyroscope)
+        ball.transform.rotation = palm.rotation;
+
+        yield return new WaitForSeconds(1f);
+
+        shot = false;
+        Preview(true, true);
+    }
+
+    public void SwitchBall(bool right)
+    {
+        //if (projection.activeInHierarchy && projection.transform.position == palm.position)
+        //{
+        if (balls.Count > 0)
+        {
+            if (right) selectedBall++;
+            else selectedBall--;
+
+            if (selectedBall < 0) selectedBall = balls.Count - 1;
+            else if (selectedBall >= balls.Count) selectedBall = 0;
+
+            projection.GetComponent<ProjectionBehaviour>().UpdateBall(balls[selectedBall]);
+        }
+        //}
+    }
+
+    public void ClearAuxiliars()
+    {
+        foreach (GameObject ball in auxiliarBalls) Destroy(ball);
+        auxiliarBalls.Clear();
+    }
+
+    public void ObtainBall(Ball ball)
+    {
+        balls.Add(ball);
+        selectedBall = balls.Count - 1;
+        projection.GetComponent<ProjectionBehaviour>().UpdateBall(balls[selectedBall]);
+    }
+
+    public void Preview(bool on, bool isPalm)
+    {
+        if (balls.Count > 0)
+        {
+            if (on)
+            {
+                if (isPalm)
+                {
+                    projection.transform.position = palm.position;
+                    projection.transform.rotation = palm.rotation;
+                    projection.transform.localScale = palm.localScale;
+                }
+                else
+                {
+                    projection.transform.position = wrist.position;
+                    projection.transform.rotation = wrist.rotation;
+                    projection.transform.localScale = wrist.localScale;
+                }
+                ShowProjection(true);
+            }
+            else ShowProjection(false);
+        }
+    }
+
+    public void SelectBall(int idx)
+    {
+        selectedBall = idx;
+        projection.GetComponent<ProjectionBehaviour>().UpdateBall(balls[selectedBall]);
+    }
+
+    #region Hands exclusive methods
+
+    public void ShowProjection(bool on)
+    {
+        projection.SetActive(on);
+    }
+
+    public void OpenPalm(bool on)
+    {
+        aiming = on;
+
+        if (on)
+        {
+            Preview(true, true);
+        }
+        else
+        {
+            SwitchBall(true);
+            Preview(true, false);
+        }
+    }
+
+    #endregion
+
+    #region Desktop exclusive methods
 
     void FixedUpdate()
     {
@@ -126,4 +297,6 @@ public class PlayerBehaviour : MonoBehaviour
             D_body.linearVelocity = new Vector3(speed * velSide, D_body.linearVelocity.y, speed * velForward);
         }
     }
+
+    #endregion
 }
--- Assets/Resources/Scripts/Components/Managers/AudioManager.cs
+++ Assets/Resources/Scripts/Components/Managers/AudioManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
 
 public class AudioManager : MonoBehaviour
 {
@@ -7,14 +8,15 @@ public class AudioManager : MonoBehaviour
 
     [Header("Music tracks")]
     [SerializeField] AudioSource musicSource;
-    [SerializeField] List<AudioClip> musicTracks = new();
+    [SerializeField] List<AudioClip> musicClips = new();
 
     [Header("Sound clips")]
+    [SerializeField] AudioSource soundSource;
     [SerializeField] List<AudioClip> soundClips = new();
 
     [Header("Voice lines")]
     [SerializeField] AudioSource voiceSource;
-    [SerializeField] List<AudioClip> voiceLines = new();
+    [SerializeField] List<AudioClip> voiceClips = new();
 
     void Awake()
     {
@@ -25,5 +27,57 @@ public class AudioManager : MonoBehaviour
         }
         else instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayMusic(bool on, int index)
+    {
+        if (on)
+        {
+            musicSource.clip = musicClips[index];
+            musicSource.Play();
+        }
+        else musicSource.Stop();
+    }
+
+    public void PlayMusic(bool on, int index, AudioSource source)
+    {
+        if (on)
+        {
+            source.clip = musicClips[index];
+            source.Play();
+        }
+        else source.Stop();
+    }
+
+    public void PlaySound(int index)
+    {
+        soundSource.clip = soundClips[index];
+        soundSource.Play();
+    }
+
+    public void PlaySound(int index, AudioSource source)
+    {
+        source.clip = soundClips[index];
+        source.Play();
+    }
+
+    public void PlayVoice(bool on, int index)
+    {
+        if (on)
+        {
+            voiceSource.clip = voiceClips[index];
+            voiceSource.Play();
+        }
+        else voiceSource.Stop();
+    }
+
+    public void PlayVoice(bool on, int index, AudioSource source)
+    {
+        if (on)
+        {
+            source.clip = voiceClips[index];
+            source.Play();
+        }
+        else source.Stop();
     }
 }
