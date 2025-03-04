a016a13a6d9d735643ccf90ea591a242ba40527d
Morsa <morsaastuta@gmail.com>
2025-03-04 04:10:49 +0100

    main menu 3

--- Assets/Resources/Scripts/Components/Controllers/Menus/Settings/SettingsAudioController.cs
+++ Assets/Resources/Scripts/Components/Controllers/Menus/Settings/SettingsAudioController.cs
@@ -9,6 +9,14 @@ public class SettingsAudioController : MonoBehaviour
     [SerializeField] Slider soundSlider;
     [SerializeField] Slider voiceSlider;
 
+    void OnEnable()
+    {
+        globalSlider.value = AudioManager.instance.globalVolume;
+        musicSlider.value = AudioManager.instance.musicVolume;
+        soundSlider.value = AudioManager.instance.soundVolume;
+        voiceSlider.value = AudioManager.instance.voiceVolume;
+    }
+
     public void SaveSettings()
     {
         AudioManager.instance.SetVolume(globalSlider.value, musicSlider.value, soundSlider.value, voiceSlider.value);
